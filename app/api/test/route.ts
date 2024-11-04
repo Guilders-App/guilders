e89b215047e7328d66a5a4a0cabf218f202462d7
Alin Ali Hassan <hassanalinali@gmail.com>
2024-11-04 22:51:27 +0100

    Removed test route

--- app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-import { saltedge } from "@/lib/providers/saltedge/client";
-import { NextResponse } from "next/server";
-
-export async function GET(_: Request) {
-  const customers = await saltedge.getCustomers();
-  return NextResponse.json(customers);
-}
