91767a120cfc81333f8a4ce191dce1555b299883
Alin Ali Hassan <hassanalinali@gmail.com>
2024-11-24 17:33:40 +0100

    Removed mentions of maybe for template privacy policy

--- app/(landing-page)/(other)/privacy-policy/page.tsx
+++ app/(landing-page)/(other)/privacy-policy/page.tsx
@@ -3,7 +3,7 @@ import { Markdown } from "@/components/landing-page/markdown";
 const privacyPolicyContent = `
 # Privacy Policy
 
-Your privacy is important to us. It is Maybe Finance, Inc.'s policy to respect your privacy and comply with any applicable law and regulation regarding any personal information we may collect about you, including across our website, maybe.co, and other sites we own and operate.
+Your privacy is important to us. It is Guilders, BV.'s policy to respect your privacy and comply with any applicable law and regulation regarding any personal information we may collect about you, including across our website, guilders.app, and other sites we own and operate.
 
 Personal information is any information about you which can be used to identify you. This includes information about you as a person (such as name, address, and date of birth), your devices, payment details, and even information about how you use a website or online service.
 
