9f8d04a8fd7e1612469b54ed6d70e50199c60c29
Alin Ali Hassan <hassanalinali@gmail.com>
2024-12-08 12:31:41 +0100

    Removed the home button and moved the logo to the left

--- components/landing-page/header.tsx
+++ components/landing-page/header.tsx
@@ -1,14 +1,5 @@
 import { Button } from "@/components/ui/button";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
-import { MoveRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -21,11 +12,6 @@ type NavigationItem = {
 };
 
 const navigationItems: NavigationItem[] = [
-  {
-    title: "Home",
-    href: "/",
-    description: "",
-  },
   // {
   //   title: "Company",
   //   description: ".",
@@ -71,69 +57,19 @@ export const Header = () => {
           : "bg-background/40"
       )}
     >
-      <div className="container relative mx-auto min-h-16 flex gap-4 flex-row lg:grid lg:grid-cols-3 items-center">
-        <div className="justify-start items-center gap-4 lg:flex hidden flex-row">
-          <NavigationMenu className="flex justify-start items-start">
-            <NavigationMenuList className="flex justify-start gap-4 flex-row">
-              {navigationItems.map((item) => (
-                <NavigationMenuItem key={item.title}>
-                  {item.href ? (
-                    <>
-                      <NavigationMenuLink href={item.href}>
-                        <Button variant="ghost">{item.title}</Button>
-                      </NavigationMenuLink>
-                    </>
-                  ) : (
-                    <>
-                      <NavigationMenuTrigger className="font-medium text-sm">
-                        {item.title}
-                      </NavigationMenuTrigger>
-                      <NavigationMenuContent className="!w-[450px] p-4">
-                        <div className="flex flex-col lg:grid grid-cols-2 gap-4">
-                          <div className="flex flex-col h-full justify-between">
-                            <div className="flex flex-col">
-                              <p className="text-base">{item.title}</p>
-                              <p className="text-muted-foreground text-sm">
-                                {item.description}
-                              </p>
-                            </div>
-                            <Button size="sm" className="mt-10">
-                              Book a call today
-                            </Button>
-                          </div>
-                          <div className="flex flex-col text-sm h-full justify-end">
-                            {item.items?.map((subItem) => (
-                              <NavigationMenuLink
-                                href={subItem.href}
-                                key={subItem.title}
-                                className="flex flex-row justify-between items-center hover:bg-muted py-2 px-4 rounded"
-                              >
-                                <span>{subItem.title}</span>
-                                <MoveRight className="w-4 h-4 text-muted-foreground" />
-                              </NavigationMenuLink>
-                            ))}
-                          </div>
-                        </div>
-                      </NavigationMenuContent>
-                    </>
-                  )}
-                </NavigationMenuItem>
-              ))}
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-        <div className="flex lg:justify-center">
+      <div className="container relative mx-auto min-h-16 flex gap-4 flex-row items-center justify-between">
+        <div className="flex justify-start">
           <Link href="/">
             <Image
               src="/assets/logo/logo_text.svg"
               alt="Guilders Logo"
-              width={180}
-              height={42}
+              height={24}
+              width={120}
               priority
             />
           </Link>
         </div>
-        <div className="flex justify-end w-full gap-4">
+        <div className="flex justify-end gap-4">
           <Link
             href={process.env.NEXT_PUBLIC_CAL_URL}
             aria-label="Book a demo call"
