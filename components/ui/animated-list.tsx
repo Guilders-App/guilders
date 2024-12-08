46c0874ce0ca34222f8bbc434ffe209ac568e981
Alin Ali Hassan <hassanalinali@gmail.com>
2024-12-08 12:27:48 +0100

    Fixed motion div

--- components/ui/animated-list.tsx
+++ components/ui/animated-list.tsx
@@ -57,7 +57,7 @@ export function AnimatedListItem({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <motion.div {...animations} layout className="mx-auto w-full">
+    <motion.div {...animations} layout>
       {children}
     </motion.div>
   );
