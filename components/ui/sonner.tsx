d863ce61c864193ff3cc914212844a4a9bc7fc3a
Alin Ali Hassan <hassanalinali@gmail.com>
2024-12-11 14:01:01 +0100

    Made sonner be above dialogs

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -11,7 +11,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="toaster group z-40"
+      className="toaster group z-51"
       toastOptions={{
         classNames: {
           toast:
