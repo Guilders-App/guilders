1a70d26b9832b6d2454bbc3a650ac321b1c1bd4b
Alin Ali Hassan <hassanalinali@gmail.com>
2024-12-13 10:03:51 +0100

    Updated toast message

--- app/(pages)/(protected)/(dashboard)/accounts/[id]/page.tsx
+++ app/(pages)/(protected)/(dashboard)/accounts/[id]/page.tsx
@@ -83,7 +83,7 @@ export default function AccountPage({
       },
       {
         onSuccess: () => {
-          toast.success("Account queued for refresh");
+          toast.success("Account refresh queued");
         },
         onError: (error) => {
           toast.error(
