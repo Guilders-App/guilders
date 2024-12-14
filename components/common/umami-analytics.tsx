bbdafffc9dff02a286b6592d33c3bbf813be6ba9
Alin Ali Hassan <hassanalinali@gmail.com>
2024-12-14 14:04:19 +0100

    Updated Umami to only work in production

--- components/common/umami-analytics.tsx
+++ components/common/umami-analytics.tsx
@@ -1,9 +1,11 @@
 export function UmamiAnalytics() {
   return (
-    <script
-      defer
-      src="https://cloud.umami.is/script.js"
-      data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
-    />
+    process.env.NODE_ENV === "production" && (
+      <script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+      />
+    )
   );
 }
