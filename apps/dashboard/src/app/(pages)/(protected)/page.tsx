73c9010cef550ddc7f54abd88dfe97488239a1e0
Alin Ali Hassan <hassanalinali@gmail.com>
2024-12-19 15:37:11 +0100

    Updated Transactions card to have a default menu item

--- apps/dashboard/src/app/(pages)/(protected)/page.tsx
+++ apps/dashboard/src/app/(pages)/(protected)/page.tsx
@@ -4,19 +4,8 @@ import { AccountsCard } from "@/components/dashboard/accounts/account-card";
 import { CategoriesCard } from "@/components/dashboard/categories/categories-card";
 import { NetWorthCard } from "@/components/dashboard/net-worth-card";
 import { TransactionsCard } from "@/components/dashboard/transactions/transactions-card";
-import { Button } from "@guilders/ui/button";
-import { ArrowRight } from "lucide-react";
-import Link from "next/link";
 
 export default function DashboardPage() {
-  const transactionsMenu = (
-    <Link href="/transactions">
-      <Button variant="secondary">
-        View All
-        <ArrowRight className="ml-2 h-4 w-4" />
-      </Button>
-    </Link>
-  );
   return (
     <div className="grid gap-6 py-8">
       <div className="grid grid-cols-1 md:grid-cols-5 gap-6">
@@ -28,10 +17,7 @@ export default function DashboardPage() {
           <AccountsCard className="h-[400px]" />
         </div>
         <div className="col-span-1 md:col-span-2">
-          <TransactionsCard
-            className="h-[400px]"
-            menuComponent={transactionsMenu}
-          />
+          <TransactionsCard className="h-[400px]" />
         </div>
       </div>
     </div>
--- apps/dashboard/src/components/dashboard/transactions/transactions-card.tsx
+++ apps/dashboard/src/components/dashboard/transactions/transactions-card.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@guilders/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@guilders/ui/card";
 import { cn } from "@guilders/ui/cn";
 import { ScrollArea, ScrollBar } from "@guilders/ui/scroll-area";
+import { ArrowRight, Link } from "lucide-react";
 import type { ReactNode } from "react";
 import { TransactionsTable } from "./transactions-table";
 
@@ -17,13 +19,24 @@ export function TransactionsCard({
   menuComponent,
   children,
 }: TransactionsCardProps) {
+  const transactionsMenu = (
+    <Link href="/transactions">
+      <Button variant="secondary">
+        View All
+        <ArrowRight className="ml-2 h-4 w-4" />
+      </Button>
+    </Link>
+  );
+
   return (
     <Card className={cn("flex flex-col", className)}>
       <CardHeader>
         <div className="flex flex-col space-y-2 md:flex-row md:items-center md:justify-between md:space-y-0">
           <CardTitle>{title}</CardTitle>
-          {menuComponent && (
+          {menuComponent ? (
             <div className="flex items-center gap-2">{menuComponent}</div>
+          ) : (
+            transactionsMenu
           )}
         </div>
       </CardHeader>
